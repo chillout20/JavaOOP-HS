57ec7b4302bb06edac3486f1fbbaeecbea38915c
chillout20 <pham.huuduc611@gmail.com>
2020-11-10 14:35:02 +0700

    Correct syntax of files

--- HW1/Task1.java
+++ HW1/Task1.java
@@ -1,12 +1,13 @@
-public class Task_1 {
+public class Task1 {
         
     private static int give_me_one() {
-        return "1";
+
+        return 1;
     }
     
     public static void Main(String[] args) {
-        int one = give_me_one()
-        int two = one * 2
-        System.out.println(two)
+        int one = give_me_one();
+        int two = one * 2;
+        System.out.println(two);
     }
 }
